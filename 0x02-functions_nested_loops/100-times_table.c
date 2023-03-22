bb9fbaf2d4ab0a07994f7159237c077d5da6da1d
myUsername <my>
2023-03-22 07:29:05 -0700

    ADD

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,50 @@
 #include "main.h"
 #include "stdio.h"
 /**
- * print_to_98 - check the code.
- * @n: Description of parameter x
+ * print_times_table - check the code.
+ * @x: Description of parameter x
  * Description
  * Return: Always 0.
  */
-void print_to_98(int n)
+void print_times_table(int x)
 {
-	int i;
+	int i, j, n;
 
-	for (i = n; i <= 98 && n <= 98; i++)
+	for (i = 0; i <= x && (x < 15 && x >= 0); i++)
 	{
-		printf("%d", i);
-		fflush(stdout);
-		if (i != 98)
+		for (j = 0; j <= x; j++)
 		{
-		_putchar(',');
-		_putchar(' ');
+			n = i * j;
+			if (n > 9)
+			{
+				if (n > 99)
+				{
+					_putchar((n / 100) + '0');
+				}
+				_putchar((n / 10) % 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(n + '0');
+			}
+			if (j < x)
+			{
+				_putchar(',');
+				if (n / 100 == 0  || n >= 99)
+				{
+					_putchar(' ');
+					if ((i * (j + 1)) / 100 == 0)
+					{
+						_putchar(' ');
+					}
+				}
+			}
 		}
+		_putchar('\n');
 	}
-	for (i = n; i >= 98 && n > 98; i--)
-	{
-		printf("%d", i);
-		fflush(stdout);
-		if (i != 98)
-		{
-		_putchar(',');
-		_putchar(' ');
-		}
-	}
-	_putchar('\n');
 }
